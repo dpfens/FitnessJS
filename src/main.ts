c1f4f25c693d9695f86cfc285ec9aec9c34a7311
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2016-09-02 16:58:55 -0400

    added reference paths for modules

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
+/// <reference path="conversion/conversion.ts" />
+/// <reference path="enums.ts" />
+/// <reference path="mets.ts"/>
 /// <reference path="person.ts" />
+/// <reference path="models/aerobic.ts" />
+/// <reference path="sport/sport.ts" />
