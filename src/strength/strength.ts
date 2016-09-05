89fa78a1e272a08405d1bf58ecb3f40662467f86
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2016-09-05 01:45:18 -0400

    changed namespace to 'strength', added JSDoc documentation

--- src/strength/strength.ts
+++ src/strength/strength.ts
@@ -1,24 +1,32 @@
 /// <reference path="../enums.ts" />
 /// <reference path="lifting.ts" />
+/// <reference path="power.ts" />
 /// <reference path="rm.ts" />
 
 namespace Fit {
 
-  export class Strength {
-    public gender: Gender;
-    public dob: Date;
-    public height: number;
-    public weight: number;
+  export namespace strength {
 
-    public lifting: Lifting;
-    public rm: RM;
-    constructor(gender: Gender, dob: Date, height: number, weight: number) {
-      this.gender = gender;
-      this.dob = dob;
-      this.height = height;
-      this.weight = weight;
-      this.lifting = new Lifting(gender, dob, height, weight);
-      this.rm = new RM(gender, dob, height, weight);
-    }
-  };
+    export class Strength {
+      public gender: Gender;
+      public dob: Date;
+      public height: number;
+      public weight: number;
+
+      public lifting: Lifting;
+      public power: Power;
+      public rm: RM;
+
+      constructor(gender: Gender, dob: Date, height: number, weight: number) {
+        this.gender = gender;
+        this.dob = dob;
+        this.height = height;
+        this.weight = weight;
+        this.lifting = new Lifting(gender, dob, height, weight);
+        this.power = new Power(gender, dob, height, weight);
+        this.rm = new RM(gender, dob, height, weight);
+      }
+    };
+
+  }
 }
