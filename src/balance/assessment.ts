e7b936691af21a7600a4711c0c7930cfc3eb93e8
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2016-09-05 01:51:32 -0400

    changed namespace to 'balance'

---
+++ src/balance/assessment.ts
@@ -0,0 +1,7 @@
+namespace Fit {
+
+  export namespace balance {
+
+  }
+
+}
--- src/balance/balance.ts
+++ src/balance/balance.ts
@@ -1,17 +1,29 @@
+/// <reference path="assessment.ts" />
+
 namespace Fit {
-  export class Balance {
-    public gender: Gender;
-    public dob: Date;
-    public height: number;
-    public weight: number;
 
-    constructor(gender: Gender, dob: Date, height: number, weight: number) {
-      this.gender = gender;
-      this.dob = dob;
-      this.height = height;
-      this.weight = weight;
+  export namespace balance {
 
+    /*
+    creates a new Balance instance
+    @class
+    @classdesc creates a class to calculate balance
+    */
+    export class Balance {
+      public gender: Gender;
+      public dob: Date;
+      public height: number;
+      public weight: number;
+
+      constructor(gender: Gender, dob: Date, height: number, weight: number) {
+        this.gender = gender;
+        this.dob = dob;
+        this.height = height;
+        this.weight = weight;
+
+      }
     }
+
   }
 
 }
