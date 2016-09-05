4fbce303518ad6cde6229a26d0e922476d4efa21
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2016-09-05 01:48:21 -0400

    changed namespace to 'flexibility'

--- src/flexibility/flexibility.ts
+++ src/flexibility/flexibility.ts
@@ -1,18 +1,29 @@
 /// <reference path="assessment.ts" />
 
 namespace Fit {
-  export class Flexibility {
-    public gender: Gender;
-    public dob: Date;
-    public height: number;
-    public weight: number;
-    
-    constructor(gender: Gender, dob: Date, height: number, weight: number) {
-      this.gender = gender;
-      this.dob = dob;
-      this.height = height;
-      this.weight = weight;
+
+  export namespace flexibility {
+
+
+    /*
+    creates a new Flexibility instance
+    @class
+    @classdesc creates a class to calculate flexibility
+    */
+    export class Flexibility {
+      public gender: Gender;
+      public dob: Date;
+      public height: number;
+      public weight: number;
+
+      constructor(gender: Gender, dob: Date, height: number, weight: number) {
+        this.gender = gender;
+        this.dob = dob;
+        this.height = height;
+        this.weight = weight;
+      }
     }
+
   }
 
 }
