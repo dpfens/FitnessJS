073cf9cb0bb669828a1fad3b87621aa98e1d902f
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2016-09-16 01:30:34 -0400

    added temperature converter

--- src/conversion/conversion.ts
+++ src/conversion/conversion.ts
@@ -1,2 +1,3 @@
 /// <reference path="system.ts" />
 /// <reference path="swain.ts"/>
+/// <reference path="temperature.ts"/>
---
+++ src/conversion/temperature.ts
@@ -0,0 +1,57 @@
+namespace Fit {
+
+  export module conversion {
+    let temperatureTable: any = {
+      C: {
+        F: function(c: number): number { return c * 9/5 + 32; },
+        K: function(c: number): number { return c + 273.15; },
+        R: function(c: number): number { return c * 9/5 + 491.67; }
+      },
+      F: {
+        C: function(f: number): number { return (f-32) * 5/9; },
+        K: function(f: number): number { return (f+459.67)*5/9; },
+        R: function(f: number): number { return f + 459.67; }
+      },
+      K: {
+        C: function(k: number): number { return k - 273.15; },
+        F: function(k: number): number { return k * 9/5 - 459.67;},
+        R: function(k: number): number { return k * 9/5; }
+      },
+      R: {
+        C: function(r: number): number { return r * 5/9 - 273.15; },
+        F: function(r: number): number { return r - 459.67; },
+        K: function(r: number): number { return r * 5/9; }
+      }
+    };
+
+    export class TemperatureConverter {
+      private value;
+      private currentUnit;
+
+      constructor(value: number, unit: string) {
+        this.value = value;
+        this.currentUnit = unit;
+      }
+
+      to = function(targetUnit: number) {
+        this.targetUnit = targetUnit;
+        return this;
+      };
+
+      val = function(): number {
+        let currentUnit = this.currentUnit.toUpperCase();
+        let targetUnit = this.targetUnit.toUpperCase();
+
+        if(!temperatureTable[currentUnit] || !temperatureTable[currentUnit][targetUnit] ) {
+          throw new Error(`Conversion not possible: &deg;${this.currentUnit} -> &deg;${this.targetUnit}`);
+        }
+        let conversionFunction = temperatureTable[currentUnit][targetUnit];
+
+        return conversionFunction(this.value);
+      };
+
+    }
+
+  }
+
+}
