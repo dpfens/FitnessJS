08cde29dd23489cdb275db21b785cea6624fb83c
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2016-11-23 13:16:13 -0500

    updated reference paths

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@
 /// <reference path="conversion/conversion.ts" />
 /// <reference path="enums.ts" />
 /// <reference path="mets.ts"/>
-/// <reference path="person.ts" />
+
+/// <reference path="balance/balance.ts" />
+/// <reference path="cardiovascular/cardiovascular.ts" />
+/// <reference path="composition/composition.ts" />
+/// <reference path="flexibility/flexibility.ts" />
+/// <reference path="strength/strength.ts" />
+
 /// <reference path="models/aerobic.ts" />
 /// <reference path="sport/sport.ts" />
