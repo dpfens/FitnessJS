acc6ea3392f0ad4b2bef156cc9ff6ce1266a34fd
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2016-11-23 13:15:09 -0500

    changed hydration class to individual methods

--- src/composition/hydration.ts
+++ src/composition/hydration.ts
@@ -8,32 +8,13 @@ namespace Fit {
 
 	export namespace composition {
 
-		/*
-		creates a new Density instance
-		@class
-		@classdesc creates a Density class for body density calculations
-		*/
-		export class Hydration {
-			private gender: Gender;
-	    private dob: Date;
-	    private height: number;
-	    private weight: number;
-			constructor(gender: Gender, dob: Date, height: number, weight: number) {
-	      this.gender = gender;
-	      this.dob = dob;
-	      this.height = height;
-	      this.weight = weight;
-	    }
-
 			/*
 			@param {Double} weight in kg
 			@returns {Double} L/day of water
 			*/
-			dailyNeeds = function(): number {
+			export function dailyWaterNeed(): number {
 			  return 0.033 * this.weight;
 			}
-
-		}
 
 	}
 
