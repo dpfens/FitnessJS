6970fd6473d1794390c0ad9688b62ecaca4fa67b
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2017-01-06 22:51:42 -0500

    fixed revised Harris Benedict formula

--- dist/fitness.js
+++ dist/fitness.js
@@ -1341,9 +1341,9 @@ var Fit;
             RevisedHB.prototype.predict = function (dob, weight, height) {
                 var age = dob.delta("years");
                 if (this.gender === Fit.Gender.Female) {
-                    return (9.5634 * weight) + (1.8496 * height) - (4.6756 * age) + 655.0955;
+                    return (9.247 * weight) + (3.098 * height) - (4.330 * age) + 447.593;
                 }
-                return (13.7516 * weight) + (5.0033 * height) - (6.7550 * age) + 66.4730;
+                return (13.397 * weight) + (4.799 * height) - (5.677 * age) + 88.362;
             };
             return RevisedHB;
         }(BMREstimator));
--- src/cardiovascular/energy.ts
+++ src/cardiovascular/energy.ts
@@ -34,9 +34,9 @@ namespace Fit {
         predict(dob: Date, weight: number, height: number): number {
           let age = dob.delta("years");
             if(this.gender === Gender.Female) {
-                return (9.5634*weight)+(1.8496*height)-(4.6756*age)+655.0955;
+                return (9.247*weight)+(3.098*height)-(4.330*age)+447.593;
             }
-            return (13.7516*weight)+(5.0033*height)-(6.7550*age)+66.4730;
+            return (13.397*weight)+(4.799*height)-(5.677*age)+88.362;
         }
 
     }
