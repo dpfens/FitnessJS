7dfa8210686fd894edb9510f3e699815fbca2455
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2018-04-01 21:43:14 -0400

    updated layouts, textfields and menus

--- tools/composition/js/layout.js
+++ tools/composition/js/layout.js
@@ -1,20 +1,13 @@
 (function() {
     var dialog = new mdc.dialog.MDCDialog(document.querySelector('#new-person-dialog')),
     shareMenuEl = document.querySelector('#share-menu'),
-    shareMenu = new mdc.menu.MDCSimpleMenu(shareMenuEl),
+    shareMenu = new mdc.menu.MDCMenu(shareMenuEl),
     shareMenuToggleButton = document.querySelector('#share-menu-button'),
     visualTabBars = new mdc.tabs.MDCTabBar(document.querySelector('#visualization-tabs')),
-    visualPanels = document.querySelector('.tab-panels'),
-    textInputElements = document.querySelectorAll('.mdc-textfield'),
-    textInputFields = [];
+    visualPanels = document.querySelector('.tab-panels');
     shareMenuToggleButton.addEventListener('click', function() {
         shareMenu.open = !shareMenu.open;
     });
-
-    // initialize text fields
-    for(var i=0;i<textInputElements.length;i++) {
-        textInputFields.push( new mdc.textfield.MDCTextfield(textInputElements[i]) );
-    }
 
     // new person dialog box
     document.querySelector('#primary-action-button').addEventListener('click', function (evt) {
