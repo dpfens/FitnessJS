2786f14d5a8f8342c28bbe8e626706988ad24f64
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2018-04-07 01:14:26 -0400

    updated FitnessJS library, and switched to use minified Material Design library

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   "globDirectory": ".",
   "globPatterns": [
-    "**/*.{svg,jpg,js,css,html,txt,json,xml}"
+    "**/*.{svg,jpg,js,css,html,txt,json,xml}",
+    "*/*/*.{svg,jpg,js,css,html,txt,json,xml}",
   ],
   "swDest": "sw.js"
 };
